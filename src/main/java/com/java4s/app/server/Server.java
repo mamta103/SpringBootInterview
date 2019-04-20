a9e2931237788dd753dead8d9a5a3bae4fdb7ae7
Mamta Kumari <32190445+mamta103@users.noreply.github.com>
2019-04-20 23:55:10 +0530

    Spring Boot – How to Change Default Context Path
    Spring Boot – How to Change Default Context Path

---
+++ src/main/java/com/java4s/app/server/Server.java
@@ -0,0 +1,15 @@
+/*package com.java4s.app.server;
+
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Server implements EmbeddedServletContainerCustomizer {
+
+	@Override
+	public void customize(ConfigurableEmbeddedServletContainer container) 
+	{
+		container.setPort(2018);
+	}
+}*/
